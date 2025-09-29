681e48efa8daad40f8b63833beec5f27767ddffc
Hadi <rittihadi@gmail.com>
2025-09-29 19:57:34 +0700

    Format oneshot.c file

--- keymap/oneshot.c
+++ keymap/oneshot.c
@@ -1,20 +1,14 @@
 #include "oneshot.h"
 
-void update_oneshot(
-    oneshot_state *state, 
-    uint16_t mod, 
-    uint16_t trigger, 
-    uint16_t keycode,
-    keyrecord_t *record
-) {
+void update_oneshot(oneshot_state *state, uint16_t mod, uint16_t trigger,
+                    uint16_t keycode, keyrecord_t *record) {
     if (keycode == trigger) {
         if (record->event.pressed) {
             if (*state == os_up_unqueued) {
                 register_code(mod);
             }
             *state = os_down_unused;
-        }
-        else {
+        } else {
             switch (*state) {
             case os_down_unused:
                 *state = os_up_queued;
@@ -27,8 +21,7 @@ void update_oneshot(
                 break;
             }
         }
-    }
-    else {
+    } else {
         if (record->event.pressed) {
             if (is_oneshot_cancel_key(keycode) && *state != os_up_unqueued) {
                 *state = os_up_unqueued;
@@ -36,33 +29,32 @@ void update_oneshot(
             }
             if (!is_oneshot_ignored_key(keycode)) {
                 switch (*state) {
-                    case os_up_queued:
-                        *state = os_up_queued_used;
-                        break;
-                    case os_up_queued_used:
-                        // Change (A down) (B down) (A up) (B up) from outputting
-                        // AB to Ab
-                        *state = os_up_unqueued;
-                        unregister_code(mod);
-                        break;
-                    default:
-                        break;
+                case os_up_queued:
+                    *state = os_up_queued_used;
+                    break;
+                case os_up_queued_used:
+                    // Change (A down) (B down) (A up) (B up) from outputting
+                    // AB to Ab
+                    *state = os_up_unqueued;
+                    unregister_code(mod);
+                    break;
+                default:
+                    break;
                 }
             }
-        }
-        else {
+        } else {
             if (!is_oneshot_ignored_key(keycode)) {
                 switch (*state) {
-                    case os_down_unused:
-                        *state = os_down_used;
-                        break;
-                    case os_up_queued:
-                    case os_up_queued_used:
-                        *state = os_up_unqueued;
-                        unregister_code(mod);
-                        break;
-                    default:
-                        break;
+                case os_down_unused:
+                    *state = os_down_used;
+                    break;
+                case os_up_queued:
+                case os_up_queued_used:
+                    *state = os_up_unqueued;
+                    unregister_code(mod);
+                    break;
+                default:
+                    break;
                 }
             }
         }
